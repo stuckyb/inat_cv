46200d4cc2da87882562c403bf084eb4cdc42d0e
stuckyb <stuckyb@flmnh.ufl.edu>
2021-04-03 17:20:06 -0400

    Added missing class.

--- dist_sampler_wrapper.py
+++ dist_sampler_wrapper.py
@@ -1,5 +1,36 @@
 from torch.utils.data import DistributedSampler
 from typing import Iterator, List, Optional, Union
+from torch.utils.data import Dataset
+
+
+class DatasetFromSampler(Dataset):
+    """Dataset to create indexes from `Sampler`.
+    Args:
+        sampler: PyTorch sampler
+    """
+
+    def __init__(self, sampler: Sampler):
+        """Initialisation for DatasetFromSampler."""
+        self.sampler = sampler
+        self.sampler_list = None
+
+    def __getitem__(self, index: int):
+        """Gets element of the dataset.
+        Args:
+            index: index of the element in the dataset
+        Returns:
+            Single element by index
+        """
+        if self.sampler_list is None:
+            self.sampler_list = list(self.sampler)
+        return self.sampler_list[index]
+
+    def __len__(self) -> int:
+        """
+        Returns:
+            int: length of the dataset
+        """
+        return len(self.sampler)
 
 
 class DistributedSamplerWrapper(DistributedSampler):
