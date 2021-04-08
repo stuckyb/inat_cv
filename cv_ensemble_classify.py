dbdeeb4cd4a2a24899ce4f9ba50b246a19bd8595
stuckyb <stuckyb@ufl.edu>
2021-04-07 22:29:00 -0400

    Many improvements to c-v ensemble classification, including new command-line args, image batch processing, and on-GPU processing.

--- cv_ensemble_classify.py
+++ cv_ensemble_classify.py
@@ -1,6 +1,6 @@
 
-from Salamander_lightning_model import SalamanderModel
-from salamander_data import getAllImagesDataset
+from model import ENModel, val_transform
+from data import UnlabeledImagesDataset, ImageCsvDataset
 import torch
 import torch.utils.data
 import torch.nn.functional as F
@@ -8,6 +8,7 @@ import csv
 import re
 from pathlib import Path
 import os.path
+from pytorch_lightning.metrics.classification import ConfusionMatrix
 from argparse import ArgumentParser
 
 
@@ -22,16 +23,53 @@ argp.add_argument(
     '-i', '--images', type=str, required=True,
     help='The path to a collection of images.'
 )
+
+argp.add_argument(
+    '-l', '--labels_csv', type=str, required=False, default='',
+    help='The path of a labels CSV file. If not provided, no accuracy or '
+    'other metrics will be calculated.'
+)
+argp.add_argument(
+    '-x', '--fnames_col', type=str, required=False, default='',
+    help='The column in the CSV file containing the image file names.'
+)
+argp.add_argument(
+    '-y', '--labels_col', type=str, required=False, default='',
+    help='The column in the CSV file containing the image labels.'
+)
+argp.add_argument(
+    '-b', '--batch_size', type=int, required=False, default=8,
+    help='The batch size.'
+)
 argp.add_argument(
     '-o', '--output', type=str, required=False, default='',
     help='The path of an output CSV file.'
 )
-argp.add_argument(
-    '-a', '--accuracy', action='store_true',
-    help='If set, record image set classifications and track accuracy.'
-)
 
 args = argp.parse_args()
+
+if args.labels_csv != '' and (args.fnames_col == '' or args.labels_col == ''):
+    exit(
+        '\nError: If a labels CSV file is provided, file and label column '
+        'names must also be provided.\n'
+    )
+
+if args.labels_csv == '':
+    imgs_ds = UnlabeledImagesDataset(args.images, transform=val_transform)
+else:
+    imgs_ds = ImageCsvDataset(
+        args.labels_csv, args.images, args.fnames_col, args.labels_col,
+        transform=val_transform
+    )
+
+dl = torch.utils.data.DataLoader(
+    imgs_ds, batch_size=args.batch_size, shuffle=False, num_workers=12
+)
+
+if torch.cuda.is_available():
+    device = torch.device('cuda:0')
+else:
+    device = torch.device('cpu')
 
 # Get the cross-validation model checkpoints.
 ckpts = []
@@ -50,9 +88,6 @@ for fold_dir in cv_dir.glob('fold_*'):
 
         ckpts.append(best_ckpt)
 
-
-all_images = getAllImagesDataset(args.images)
-
 if args.output != '':
     writer = csv.DictWriter(
         open(args.output, 'w'),
@@ -68,28 +103,57 @@ models = []
 with torch.no_grad():
     for i, ckpt in enumerate(ckpts):
         print(f'Loading best model from fold {i}...')
-        model = SalamanderModel.load_from_checkpoint(ckpt, lr=0.001)
+        model = ENModel.load_from_checkpoint(ckpt, lr=0.001, n_classes=2)
+        model.to(device)
         model.eval()
         models.append(model)
 
-    i = 0
+    c_mat = ConfusionMatrix(num_classes=2)
+    img_cnt = 0
     correct_cnt = 0
-    for img, label in all_images:
-        imgfile = all_images.samples[i][0]
 
+    for batch, labels in dl:
+        # Get the predictions for the batch from each model.
         outputs = []
         for model in models:
-            output = model(torch.unsqueeze(img, 0))
+            output = model(batch.to(device)).cpu()
             output = F.softmax(output, 1)
-            outputs.append(torch.squeeze(output))
+            #print(output)
+            outputs.append(output)
 
-        outputs = torch.stack(outputs)
-        #print(outputs)
-        model_avg = torch.mean(outputs, 0)
-        #print(model_avg)
-        p_label = torch.max(model_avg, 0).indices
-        print(label, int(p_label), imgfile)
-        #print(p_labels, all_images.samples[i])
+        # For each image in the batch, average the model predictions.
+        p_labels = torch.zeros_like(labels)
+        for i in range(len(labels)):
+            # Gather the predictions for this image.
+            img_preds = []
+            for j in range(len(models)):
+                img_preds.append(outputs[j][i,:])
+
+            # Calculate the ensemble prediction for this image.
+            img_preds = torch.stack(img_preds)
+            #print(img_preds)
+            model_avg = torch.mean(img_preds, 0)
+            #print(model_avg)
+            p_label = torch.max(model_avg, 0).indices
+            p_labels[i] = p_label
+            #print(label, int(p_label), imgfile)
+            #print(p_labels, all_images.samples[i])
+
+        #print(p_labels)
+
+        # Make adjustments to the ground truth labels for unclassifiable and
+        # mixed images.
+        adj_labels = torch.zeros_like(labels)
+        for i, label in enumerate(labels):
+            if imgs_ds.classes[label] == 'U':
+                # An unclassifiable image; the adjustment should be the
+                # opposite of the prediction (i.e., it is always wrong).
+                adj_labels[i] = abs(p_labels[i] - 1)
+            elif imgs_ds.classes[label] == 'B':
+                # An image containing both color morphs.
+                adj_labels[i] = p_labels[i]
+            else:
+                adj_labels[i] = labels[i]
 
         if writer is not None:
             rowout['file'] = os.path.basename(imgfile)
@@ -98,14 +162,13 @@ with torch.no_grad():
             rowout['1'] = float(model_avg[1])
             writer.writerow(rowout)
 
-        if args.accuracy:
-            if label == p_label:
-                correct_cnt += 1
-            else:
-                print('  MISTAKE:', all_images.samples[i])
-        i += 1
-        if args.accuracy:
-            print('Cumulative accuracy:', correct_cnt / i, i)
+        c_mat.update(p_labels, adj_labels)
+        img_cnt += len(adj_labels)
+        correct_cnt += sum(p_labels == adj_labels)
+        print(correct_cnt / img_cnt, img_cnt)
 
-print(i, 'total images processed.')
+print('\nTotal images:', img_cnt)
+print('Accuracy:', float(correct_cnt / img_cnt))
+print('Confusion matrix:')
+print(c_mat.compute().numpy())
 
